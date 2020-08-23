3012f388bd7743fa478aa3dee0026eae707688fb
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-08-23 09:18:32 -0300

    Secao 16 - Aula 210 - No meio da aula.

---
+++ lib/screen/task_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding:
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+        child: Column(
+          children: [Icon(Icons.list), Text('Todoey')],
+        ),
+      ),
+    );
+  }
+}
